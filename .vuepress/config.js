c6383f540ffbeacc83ed38a47ec0037b08db5d07
rhange <rhange0511@gmail.com>
2021-06-02 18:33:40 +0900

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,6 @@ module.exports = {
 	base: "/TIL/",
 	dest: "build",
 	themeConfig: {
-		logo: "https://avatars.githubusercontent.com/u/46353755?v=4",
 		nav: [{ text: "Home", link: "/" }],
 		sidebar: "auto", // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
 	},
