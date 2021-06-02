23fbf7dd481ddc7ab15ae0945cd1870916714a4f
rhange <rhange0511@gmail.com>
2021-06-02 18:27:08 +0900

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	title: "Today Rhange Learned",
+	description: "vuepress로 만든 문서입니다.",
+	themeConfig: {
+		logo: "https://avatars.githubusercontent.com/u/46353755?v=4",
+		nav: [{ text: "Home", link: "/" }],
+		sidebar: "auto", // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+	},
+};
