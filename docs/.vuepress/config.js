cdd9167f4956f8eef7d37f86ad7230ef4c4cee2c
rhange <rhange0511@gmail.com>
2021-06-27 21:00:10 +0900

    added BMC

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,11 @@ module.exports = {
 			"https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory4.daumcdn.net%2Ftistory%2F1952831%2Fattach%2F8446c51039b946da8fc0b47528768cc6",
 		nav: [
 			{ text: "Github", link: "https://github.com/rhange" },
-			{ text: "Blog", link: "https://rhange.gatsbyjs.io/yt" },
+			{ text: "Blog", link: "https://rhange.gatsbyjs.io" },
+			{
+				text: "☕ Buy Me A Coffee",
+				link: "https://www.buymeacoffee.com/rhange",
+			},
 		],
 		sidebar,
 		smoothScroll: true,
